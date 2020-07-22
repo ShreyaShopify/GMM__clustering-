08ca753ba2df239b41e6d76b9d8d5cf2013684ad
Shreya <64823833+ShreyaShopify@users.noreply.github.com>
2020-07-22 14:16:21 +0000

    Update main.py

--- Clustering Model/main.py
+++ Clustering Model/main.py
@@ -1,2 +1,14 @@
-# Code for GMM 
-# Class GaussianMixture
+from pyspark.ml.clustering import PowerIterationClustering
+
+df = spark.createDataFrame([
+    (0, 1, 1.0),
+    (0, 2, 1.0),
+    (1, 2, 1.0),
+    (3, 4, 1.0),
+    (4, 0, 0.1)
+], ["src", "dst", "weight"])
+
+pic = PowerIterationClustering(k=2, maxIter=20, initMode="degree", weightCol="weight")
+
+# Shows the cluster assignment
+pic.assignClusters(df).show()e
