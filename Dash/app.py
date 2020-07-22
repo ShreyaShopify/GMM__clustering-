30e9edea7b24ed641238b596339ee2c82ca566ab
Shreya <64823833+ShreyaShopify@users.noreply.github.com>
2020-07-22 18:43:36 +0000

    Update app.py

--- Dash/app.py
+++ Dash/app.py
@@ -11,8 +11,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# Figure out how to connect this to the spark session 
+# todo: how do we do this directly from spark 
 df = pd.read_csv('/Users/shreyaprasad/Downloads/Shop_dimension_data.csv')
+df_gmv = pd.read_csv('/Users/shreyaprasad/Downloads/gmv_per_month_per_country.csv')
+df_test = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 available_countries = df['Shop Country Name'].unique()
 
 def generate_table(dataframe, max_rows=10):
@@ -35,18 +37,41 @@ html.Label('Country'),
         value=['United States'],
         multi=True
     ),
+    dcc.Graph(id='graph-with-country'),
     dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df_test['year'].min(),
+        max=df_test['year'].max(),
+        value=df_test['year'].min(),
+        marks={str(year): str(year) for year in df_test['year'].unique()},
+        step=None
+    ),
     html.H4(children='Shop Dimension Data'),
     generate_table(df)
 ])
 
 @app.callback(
-    Output('graph-with-slider', 'figure'),
+    Output('graph-with-country', 'figure'),
     [Input('country_selector', 'value')])
 def update_figure(selected_country):
     filtered_df = df[df['Shop Country Name'] == selected_country]
     fig = px.bar(filtered_df, x="Shop Country Name", y="Shop Commerce Background",
                      size="pop", #color="continent", hover_name="country",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df_test[df_test.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
                      log_x=True, size_max=55)
 
     fig.update_layout(transition_duration=500)
